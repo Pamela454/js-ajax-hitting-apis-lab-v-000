5b1046e0b36267d67110c2c622352dc81d6f5f52
Pamela Frieze <pfrieze@gmail.com>
2019-03-11 22:22:03 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -69,5 +69,17 @@ function getBranches(el) {
 
 function displayBranches() {
    const branches = JSON.parse(this.responseText);
-   
+   const branchesList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><h3>' +
+        commit.commit.author.name +
+        ' (' +
+        commit.author.login +
+        ') </h3> ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
 }
